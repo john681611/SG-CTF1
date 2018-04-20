0d4a012f9cf27963dd97f648962de96ceee6af98
john681611 <john.harvey@marks-and-spencer.com>
2018-04-20 14:02:41 +0100

    added more stuff

--- routes.js
+++ routes.js
@@ -1,13 +1,36 @@
 const express = require('express');
 const router = new express.Router();
+const users = require('./page/users.json');
 
 
 router.get('/', function (req, res) {
     res.renderMin('index.ejs', {});
 });
 
-router.get('/admin', function (req, res) {
-    res.renderMin('edit.ejs', {});
+router.get('/member/:user', function (req, res) {
+    console.log(req.query);
+    const loginFlag = req.query.logged? "FLAG:":"";
+    res.renderMin('member.ejs', {loginFlag: loginFlag});
+});
+
+router.post('/member', function (req, res) {
+   if(req.body.admin && req.body.role !== 'Admin'){
+    return res.send('FLAG:');
+   } 
+   return res.renderMin('index.ejs', {});
+});
+
+router.post('/login', function (req, res) {
+    if(req.body.user && req.body.password){
+        if(users.find(user => user.user === req.body.user).password == req.body.password){
+            if(users.find(user => user.user === req.body.user).admin){
+                return  res.renderMin('admin.ejs', {members:users});
+            } else {
+                 res.redirect(`/member/${req.body.user}?logged=true`);
+            }
+        }
+    }
+    res.redirect('/');
 });
 
 
